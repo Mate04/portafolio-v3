b0e2c8c1b4922ffe5b789a9f53b28874d1d1df5d
Mateo Elias Maldonado <98717@sistemas.frc.utn.edu.ar>
2025-07-12 19:09:58 -0300

    feat: agregado seccion de idiomas y educacion

--- src/data/info.type.ts
+++ src/data/info.type.ts
@@ -1,36 +1,41 @@
 export type DataJSON = {
-    descripcionPerfil: string;
-    puesto: string;
-    contacto: {
-        ciudadPais: string;
-        redesSociales: Array<{
-            nombre: string;
-            link: string;
-        }>;
-    };
-    experienciasLaborales: ExperienciaLaboral;
-    educacion: Array<{
-        titulo: string;
-        institucion: string;
-        fechaDesde: string;
-        fechaHasta: string;
-        certificadoPDF: string;
+  descripcionPerfil: string;
+  puesto: string;
+  contacto: {
+    ciudadPais: string;
+    redesSociales: Array<{
+      nombre: string;
+      link: string;
     }>;
-    habilidades: Array<{
-        nombre: string;
-        area: string;
-    }>;
-    idiomas: Array<{
-        idioma: string;
-        nivel: string;
-    }>;
+  };
+  experienciasLaborales: ExperienciaLaboral;
+  educacion: Education;
+  habilidades: Array<{
+    nombre: string;
+    area: string;
+  }>;
+  idiomas: Languaje;
 };
 
 export type ExperienciaLaboral = Array<{
-        empresa: string;
-        cargo: string;
-        fechaDesde: string;
-        fechaHasta: string;
-        descripcionPuesto: string;
-        habilidades: Array<string>;
-    }>
+  empresa: string;
+  cargo: string;
+  fechaDesde: string;
+  fechaHasta: string;
+  descripcionPuesto: string;
+  habilidades: Array<string>;
+}>;
+
+export type Languaje = Array<{
+  idioma: string;
+  nivel: string;
+}>;
+
+export type Education = Array<{
+  titulo: string;
+  institucion: string;
+  fechaDesde: string;
+  fechaHasta: string;
+  certificadoPDF: string;
+  descripcion?:string;
+}>;
